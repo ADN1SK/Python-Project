f17a0a90190436e3f9dc52f1855fb4565d33b1bb
Adam <adanmoha0987@gmail.com>
2026-04-06 23:39:30 +0300

    my first python project

---
+++ main.py
@@ -0,0 +1,13 @@
+# function to calculate the sphere volume
+def volume_of_sphere(radius):
+    pi = 3.14159
+    volume = (4/3) * pi * radius ** 3
+    return volume
+print(volume_of_sphere(5))
+
+# function to calculate the area of a circle
+def area_of_circle(radius):
+    pi = 3.14159
+    area = pi * radius ** 2
+    return area
+print(area_of_circle(5))
