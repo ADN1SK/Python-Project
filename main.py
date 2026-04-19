82e266f014ac2671a1893f0b9511b4b0ae69a358
Adam <adanmoha0987@gmail.com>
2026-04-19 10:47:20 +0300

    new

--- main.py
+++ main.py
@@ -1,73 +1,67 @@
-
-# function to calculate the sphere volume
-import string
-def volume_of_sphere(radius):
-    pi = 3.14159
-    volume = (4/3) * pi * radius ** 3
-    return volume
-print(volume_of_sphere(9))
-
-# function to calculate the area of a circle
-def area_of_circle(radius):
-    pi = 3.14159
-    area = pi * radius ** 2
-    return area
-print(area_of_circle(5))
-
-# function to calculate the area of a rectangle
-def area_of_rectangle(length, width):
-    area = length * width
-    return area
-print(area_of_rectangle(5, 3))
-# function that counts the number of words in a string
-def count_words(string):
-    words = string.split()
-    return len(words)
-print(count_words("Hello world! This is a test and my name is Adam."))
+# # function that counts the number of words in a string
+# def count_words(string):
+#     words = string.split()
+#     return len(words)
+# print(count_words("Hello world! This is a test and my name is Adam."))
 
 
-fruit = "banana"
-if 'n' in fruit:
-    print('found it')
+# xfile= open('Life Expectancy Data.csv')
+# count=0
+# for line in xfile:
+#     count= count +1
+#     print('line count: ',count)
 
-
-greet= "hello WORLD"
-zap= greet.upper()
-print(zap)
-
-greet="hello world"
-nstr=greet.replace("world","adan")
-print(nstr)
-
-
-xfile= open('MONGODB adammoha0987 db.txt')
-count=0
-for line in xfile:
-    count= count +1
-    print('line count: ',count)
-
-xfile= open('MONGODB adammoha0987 db.txt')
-inp= xfile.read()
-print(len(inp))
-print(inp[:20])
-
-xfile=open('MONGODB adammoha0987 db.txt')
-for line in xfile:
-    if line.startswith("adam"):
-        print(line)
+# xfile= open('Life Expectancy Data.csv')
+# inp= xfile.read()
+# print(len(inp))
+# print(inp[:20])
         
-xfile=open('MONGODB adammoha0987 db.txt')
-for line in xfile:
-    line=line.lstrip()
-    if not line.startswith("adam"):
-        continue
-    print(line)
+# xfile=open('MONGODB adammoha0987 db.txt')
+# for line in xfile:
+#     line=line.lstrip()
+#     if not line.startswith("adam"):
+#         continue
+#     print(line)
 
 
-fname= input("enter the file name:")
-fhand=open (fname)
-count=0
-for line in fhand:
-    if line.startswith("m"):
-        count= count + 1
-print("there were", count, "m line in", fname)
+# fname= input("enter the file name:")
+# fhand=open(fname)
+# count=0
+# for line in fhand:
+#     if line.startswith("m"):
+#         count= count + 1
+# print("there were", count, "m line in", fname)
+
+# fname= input('enter file:')
+# if len(fname) < 1:fname='test.txt'
+# hand= open(fname)
+# for line in hand:
+#     line= line.rstrip()
+#     #print(line)
+#     wds=line.split()
+#     print(wds)
+#     for w in wds:
+#         print(w)
+# import socket 
+# mysock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('data.pr4e.org', 80))
+# cmd= 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+# mysock.send(cmd)
+# while True:
+#     data= mysock.recv(512)
+#     if len(data) < 1:
+#         break
+#     print(data.decode(),end='')
+# mysock.close()
+# print(ord('H'))
+# print(ord('e'))
+# print(ord('\n'))
+name= input('enter file:')
+handle= open(name)
+text= handle.read()
+words= text.split()
+counts= dict()
+for word in words:
+    counts[word]= counts.get(word,0) 
+
+# ord() function returns the Unicode code point for a given character. In this case, it returns the code points for 'H', 'e', and the newline character '\n'.
